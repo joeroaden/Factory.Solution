6903194e9662942f6c6cb2b0f8bde27895a27ce2
joeroaden <106051349+joeroaden@users.noreply.github.com>
2022-08-05 10:01:49 -0700

    update models for engineer, machine and enginnermachine

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+namespace Factory.Models
+{
+    public class Engineer
+    {
+        public Engineer()
+        {
+            this.JoinEntities = new HashSet<EngineerMachine>();
+
+        }
+
+        public int EngineerId { get; set; }
+        public string Name { get; set; }
+        public string Status {get; set;}
+       
+
+        public virtual ICollection<EngineerMachine> JoinEntities { get;}
+
+    }
+}
